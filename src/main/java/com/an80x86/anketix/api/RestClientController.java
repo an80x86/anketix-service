e768bdd7cf9ca5b56e3b486fb5c547e5e5b54df0
mustafa bayer <an80x86@gmail.com>
2020-01-20 01:32:00 +0300

    info eklendi

--- src/main/java/com/an80x86/anketix/api/RestClientController.java
+++ src/main/java/com/an80x86/anketix/api/RestClientController.java
@@ -1,6 +1,7 @@
 package com.an80x86.anketix.api;
 
 import com.an80x86.anketix.dto.LoginDto;
+import com.an80x86.anketix.dto.TokenDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.Random;
 
 @RestController
 @RequestMapping("/api")
@@ -37,9 +40,42 @@ public class RestClientController {
 		map.add("parola", "1");
 
 		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
-		ResponseEntity<String> response = restTemplate.postForEntity( webUrl + "assos-login", request , String.class );
+		ResponseEntity<String> response = restTemplate.postForEntity( webUrl + "assos-login", request, String.class );
 
 		return response;
 	}
 
+	@PostMapping(path = "/info")
+	public ResponseEntity<String> setInformation(@RequestBody TokenDto token) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+		MultiValueMap<String, String> map= new LinkedMultiValueMap<String, String>();
+		map.add("cmd", "EARSIV_PORTAL_KULLANICI_BILGILERI_GETIR");
+		map.add("callid", getRandomString() + "-11");
+		map.add("pageName", "RG_KULLANICI");
+		map.add("token", token.getToken());
+		map.add("jp", "{}");
+
+		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
+		ResponseEntity<String> response = restTemplate.postForEntity( webUrl + "dispatch", request, String.class );
+
+		return response;
+	}
+
+	private String getRandomString() {
+
+		int leftLimit = 97; // letter 'a'
+		int rightLimit = 122; // letter 'z'
+		int targetStringLength = 13;
+		Random random = new Random();
+		StringBuilder buffer = new StringBuilder(targetStringLength);
+		for (int i = 0; i < targetStringLength; i++) {
+			int randomLimitedInt = leftLimit + (int)
+					(random.nextFloat() * (rightLimit - leftLimit + 1));
+			buffer.append((char) randomLimitedInt);
+		}
+		String generatedString = buffer.toString();
+		return generatedString;
+	}
 }
---
+++ src/main/java/com/an80x86/anketix/dto/TokenDto.java
@@ -0,0 +1,10 @@
+package com.an80x86.anketix.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TokenDto {
+    private String token;
+}
